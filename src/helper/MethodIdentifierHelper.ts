c2f15c6f78cf5cf0f662ebc0636b2a51dd89f1d3
Kay Makowsky <kay.makowsky@hitabis.de>
2025-07-21 18:00:44 +0200

    improve MethodIdentifierHelper

--- src/helper/MethodIdentifierHelper.ts
+++ src/helper/MethodIdentifierHelper.ts
@@ -2,18 +2,7 @@ import {
 	ProgramStructureTreeType,
 	SourceNodeIdentifierPart_string
 } from '@oaklean/profiler-core/dist/src/types'
-import {
-	LangInternalSourceNodeIdentifierRegex,
-	MethodDefinitionRegex,
-	ClassDeclarationRegex,
-	FunctionDeclarationRegex,
-	FunctionExpressionRegex,
-	ConstructorDeclarationRegex
-} from '@oaklean/profiler-core/dist/src/constants/SourceNodeRegex'
 import { SourceNodeIdentifierHelper } from '@oaklean/profiler-core/dist/src/helper/SourceNodeIdentifierHelper'
-
-const allowedMethodTypes = ['function', 'functionExpression', 'method', 'method@static', 'class', 'constructor']
-
 
 const IDENTIFIER_TYPE_ABBREVIATION: Record<ProgramStructureTreeType, string> = {
 	[ProgramStructureTreeType.Root]: '(r)',
@@ -28,15 +17,17 @@ const IDENTIFIER_TYPE_ABBREVIATION: Record<ProgramStructureTreeType, string> = {
 	[ProgramStructureTreeType.IfElseStatement]: '(s)',
 	[ProgramStructureTreeType.SwitchStatement]: '(s)',
 	[ProgramStructureTreeType.SwitchCaseClause]: '(s)',
-	[ProgramStructureTreeType.ObjectLiteralExpression]: '(s)',
+	[ProgramStructureTreeType.ObjectLiteralExpression]: '(s)'
 }
 export class MethodIdentifierHelper {
-
-	static getShortenedIdentifier(identifier: SourceNodeIdentifierPart_string): string {
-		const result = SourceNodeIdentifierHelper.parseSourceNodeIdentifierPart(identifier)
+	static getShortenedIdentifier(
+		identifier: SourceNodeIdentifierPart_string
+	): string {
+		const result =
+			SourceNodeIdentifierHelper.parseSourceNodeIdentifierPart(identifier)
 		if (result === null) {
 			return 'undefined'
 		}
-		return `${IDENTIFIER_TYPE_ABBREVIATION[result.type]}&nbsp;${result.name}`
+		return `${IDENTIFIER_TYPE_ABBREVIATION[result.type]} ${result.name}`
 	}
 }
