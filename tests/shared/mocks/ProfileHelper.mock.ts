8ec61542f95e64fc9323bf8a96f536e3ff6f7365
Kay Makowsky <kay.makowsky@hitabis.de>
2025-08-04 15:42:05 +0200

    fix tests

--- tests/shared/mocks/ProfileHelper.mock.ts
+++ tests/shared/mocks/ProfileHelper.mock.ts
@@ -7,21 +7,27 @@ import { Color } from '../../../src/types/color'
 
 const WORKSPACE_PATH_UNIFIED = new UnifiedPath(WORKSPACE_PATH)
 
-
 export const stub_returnSettingsPath = () => {
-	const returnSettingsPathStub = sinon.stub(ProfileHelper.prototype, 'returnSettingsPath')
-	returnSettingsPathStub.returns(WORKSPACE_PATH_UNIFIED.join('.vscode', 'settings.json'))
+	const returnSettingsPathStub = sinon.stub(
+		ProfileHelper.prototype,
+		'returnSettingsPath'
+	)
+	returnSettingsPathStub.returns(
+		WORKSPACE_PATH_UNIFIED.join('.vscode', 'settings.json')
+	)
 	return returnSettingsPathStub
 }
 
 export const stub_readProfiles = () => {
 	const readProfilesStub = sinon.stub(ProfileHelper.prototype, 'readProfiles')
-	readProfilesStub.returns([
-		{
-			'name': 'Profile 1',
-			'color': Color.Red,
-			'measurement': 'profilerHits'
-		}
-	])
+	readProfilesStub.returns({
+		profiles: [
+			{
+				name: 'Profile 1',
+				color: Color.Red,
+				measurement: 'profilerHits'
+			}
+		]
+	})
 	return readProfilesStub
 }
